f75ef6f8cd51bd0c128b307924aa548fd9626237
Farook13 <umarulfarook787@gmail.com>
2025-02-26 15:36:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,11 +97,19 @@ OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Faroo_bruh')
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
 PM_SEARCH = bool(environ.get('PM_SEARCH', True))
 BUTTON_MODE = bool(environ.get('BUTTON_MODE', True))
-IMDB = bool(environ.get('IMDB', True))
+MAX_BTN = bool(environ.get('MAX_BTN', True))
+IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
+IMDB = bool(environ.get('IMDB', False))
+AUTO_FFILTER = bool(environ.get('AUTO_FFILTER', False))
+AUTO_DELETE = bool(environ.get('AUTO_DELETE', False))
 LONG_IMDB_DESCRIPTION = bool(environ.get("LONG_IMDB_DESCRIPTION", False))
+SPELL_CHECK_REPLY = bool(environ.get("SPELL_CHECK_REPLY", False))
 MELCOW_NEW_USERS = bool(environ.get('MELCOW_NEW_USERS', True))
-NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+PROTECT_CONTENT = bool(environ.get('PROTECT_CONTENT', False))
+PUBLIC_FILE_STORE = bool(environ.get('PUBLIC_FILE_STORE', False))
+NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+
 
 
 # Token Verification Info :
